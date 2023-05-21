ba3bbe046926a42db2abcdd1601d6151f632130b
Eric Mphande <Eric Mphande@DESKTOP-G9L0B97>
2023-05-21 19:52:41 +0200

    Add project files.

---
+++ Cook Book/Ingrediant.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Initialize ingredianr delegate.
+delegate void IngredientDelegate(string name, int quantity, string unit);
+
+namespace Cook_Book
+{
+    class Ingrediant
+
+    {
+        public void IngrediantCapture()
+        {
+            // Prompt the user to enter the number of ingredients.
+            Console.WriteLine("Enter the number of ingredients:");
+
+            // Read the number of ingredients entered by the user.
+            int numIngredients = int.Parse(Console.ReadLine());
+
+            // Create an instance of the IngredientDelegate delegate and assign it to a variable called ingredientDelegate.
+            IngredientDelegate ingredientDelegate = new IngredientDelegate(AddIngredient);
+
+            // Loop through each ingredient and prompt the user to enter its name, quantity, and unit of measurement.
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine("Enter the name of ingredient " + (i + 1) + ":");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter the quantity of ingredient " + (i + 1) + ":");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the unit of measurement for ingredient " + (i + 1) + ":");
+                string unit = Console.ReadLine();
+
+                // Call the AddIngredient method using the ingredientDelegate delegate and pass in the name, quantity, and unit of measurement for each ingredient.
+                ingredientDelegate(name, quantity, unit);
+            }
+
+            // Prompt the user to enter the number of steps.
+            Console.WriteLine("Enter the number of steps:");
+
+            // Read the number of steps entered by the user.
+            int numSteps = int.Parse(Console.ReadLine());
+
+            // Create an array called steps with a length equal to numSteps.
+            string[] steps = new string[numSteps];
+
+            // Loop through each step and prompt the user to enter it.
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine("Enter step " + (i + 1) + ":");
+                steps[i] = Console.ReadLine();
+            }
+
+            // Print out all of the ingredients using the PrintIngredient method.
+            Console.WriteLine("Recipe:");
+            ingredientDelegate = new IngredientDelegate(PrintIngredient);
+            ingredientDelegate(null, 0, null);
+
+            // Print out all of the steps using string.Join method.
+            Console.WriteLine(string.Join(", ", steps));
+        }
+
+        // Method that adds an ingredient to a recipe.
+        static void AddIngredient(string name, int quantity, string unit)
+        {
+            Console.WriteLine(quantity + " " + unit + " of " + name);
+        }
+
+        // Method that prints out all of the ingredients in a recipe.
+        static void PrintIngredient(string name, int quantity, string unit)
+        {
+        }
+    }
+}
---
+++ Cook Book/Program.cs
@@ -0,0 +1,53 @@
+﻿using Cook_Book;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Display menu options
+        Console.WriteLine("Select an option:");
+        Console.WriteLine("1. New Recipe.");
+        Console.WriteLine("2. Display Recipe.");
+        Console.WriteLine("3. Change Serving Size.");
+        Console.WriteLine("4. Reset Scale.");
+        Console.WriteLine("5. Clear All Recipes.");
+        Console.WriteLine("6. Close.");
+
+        // Read user input
+        string input = Console.ReadLine();
+
+        // Process user input using switch statement
+        switch (input)
+        {
+            case "1":
+                Console.WriteLine("New Recipe selected");
+                Ingrediant ingrediant = new Ingrediant();
+                ingrediant.IngrediantCapture();
+                Console.WriteLine("New ingrediant created!");
+                Application.Restart();
+                Process.GetCurrentProcess().Kill();
+                break;
+            case "2":
+                Console.WriteLine("Display Recipe selected");
+                break;
+            case "3":
+                Console.WriteLine("Change Serving Size selected");
+                break;
+            case "4":
+                Console.WriteLine("Reset Scale selected");
+                break;
+            case "5":
+                Console.WriteLine("Clear All Recipes selected");
+                break;
+            case "6":
+                Console.WriteLine("Close selected");
+                break;
+            default:
+                Console.WriteLine("Invalid selection");
+                break;
+        }
+    }
+}
